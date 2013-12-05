bdc43c40e50305f2e12f29c5ff7e8b376b73d90b
larrynung <larry.nung@TWDT092.xuenn.com>
2013-12-05 15:26:51 +0800

    init vcs

---
+++ Source/RDPDemo/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace RDPDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMsTscAxNotSafeForScripting1 = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxDomain = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axMsTscAxNotSafeForScripting1
+            // 
+            this.axMsTscAxNotSafeForScripting1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMsTscAxNotSafeForScripting1.Enabled = true;
+            this.axMsTscAxNotSafeForScripting1.Location = new System.Drawing.Point(0, 69);
+            this.axMsTscAxNotSafeForScripting1.Name = "axMsTscAxNotSafeForScripting1";
+            this.axMsTscAxNotSafeForScripting1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsTscAxNotSafeForScripting1.OcxState")));
+            this.axMsTscAxNotSafeForScripting1.Size = new System.Drawing.Size(683, 412);
+            this.axMsTscAxNotSafeForScripting1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbxDomain);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbxPassword);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tbxUser);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbxIP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(683, 69);
+            this.panel1.TabIndex = 2;
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Location = new System.Drawing.Point(69, 12);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(148, 20);
+            this.tbxIP.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "User";
+            // 
+            // tbxUser
+            // 
+            this.tbxUser.Location = new System.Drawing.Point(203, 43);
+            this.tbxUser.Name = "tbxUser";
+            this.tbxUser.Size = new System.Drawing.Size(93, 20);
+            this.tbxUser.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(361, 43);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(106, 20);
+            this.tbxPassword.TabIndex = 7;
+            this.tbxPassword.UseSystemPasswordChar = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(473, 41);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Domain";
+            // 
+            // tbxDomain
+            // 
+            this.tbxDomain.Location = new System.Drawing.Point(69, 43);
+            this.tbxDomain.Name = "tbxDomain";
+            this.tbxDomain.Size = new System.Drawing.Size(93, 20);
+            this.tbxDomain.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 481);
+            this.Controls.Add(this.axMsTscAxNotSafeForScripting1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting axMsTscAxNotSafeForScripting1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxIP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxDomain;
+    }
+}
+
---
+++ Source/RDPDemo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxMSTSCLib;
+using MSTSCLib;
+
+namespace RDPDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectRDP(axMsTscAxNotSafeForScripting1, tbxIP.Text, tbxDomain.Text, tbxUser.Text, tbxPassword.Text);
+        }
+
+        private void ConnectRDP(AxMsTscAxNotSafeForScripting rdp, string ip, string domain, string user, string password)
+        {
+            if (rdp.Connected == 1)
+                rdp.Disconnect();
+
+            rdp.Server = ip;
+            rdp.Domain = domain;            
+            rdp.UserName = user;
+            IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+            secured.ClearTextPassword = password;
+
+            rdp.Connect();
+        }
+    }
+}
